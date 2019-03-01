40692c70ab26a52d652227686a324ee9ad9a159f
Mohammed Ibrahim <mmohame4@lakeheadu.ca>
2019-03-01 14:37:09 -0500

    InAppBrowser crash

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  constructor(private iab: InAppBrowser){
+  }
+
+  openLink() {
+    const browser = this.iab.create('https://ionicframework.com');
+    browser.show();
+  }
+}
